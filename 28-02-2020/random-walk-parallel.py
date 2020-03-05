8a52b721517fec788267969271ec43a876c73e58
HPCE5035 <rohithsrinivaas@gmail.com>
2020-03-05 14:02:45 +0530

    Changed the random-walk code

--- 28-02-2020/random-walk-parallel.py
+++ 28-02-2020/random-walk-parallel.py
@@ -9,10 +9,10 @@ rank = comm.Get_rank()
 
 P = 10;
 num_particles = int(np.ceil(P/nprocs))
-x_width = P/nprocs
+x_width = float(P)/nprocs
 
 
-num_iters = 1;
+num_iters = 10;
 step_length = (x_width/4);
 
 
@@ -29,26 +29,73 @@ class particle:
 		return self.loc
 	def move(self,x):
 		self.loc = self.loc + x;
+
 plist = []
 for i in range(num_particles):
-	percent = np.random.random()
-	loc = x_width*(rank*(1 - percent) + (rank+1)*percent)
-	plist.append(particle(i+rank*(num_particles),loc))	
-	
+	loc = np.random.uniform(rank*x_width,(rank+1)*x_width)
+	plist.append(particle(i+rank*(num_particles),loc))
 
+leaving_right  = [];
+leaving_left = [];
+incoming_right = [];
+incoming_left = [];
+
+target_particle = np.random.randint(num_particles)
+ 
 for t in range(num_iters):
-	for i in range(num_particles):
-		plist[i].move(np.random.uniform(-step_length,step_length,1)[0])
-		print(plist[i].show_pld(),plist[i].show_loc(),rank)
+	if rank < nprocs:
+		for i in range(len(plist)):
+			plist[i].move(np.random.uniform(-step_length,step_length))
+			if plist[i].show_loc() >= rank*x_width and plist[i].show_loc() < (rank+1)*x_width :
+				continue;
+			elif plist[i].show_loc() >= (rank + 1)*x_width:
+				leaving_right.append(plist[i]);	 
+			elif plist[i].show_loc() < rank*x_width:
+				leaving_left.append(plist[i]);
 
-
-		if plist[i].show_loc() > (rank+1)*x_width and plist[i].show_loc() <= (rank+2)*x_width and rank != nprocs - 1:
-			comm.send(plist[i],dest = rank + 1)
-			addon = comm.recv(source = rank - 1)		
-		if plist[i].show_loc() < 0:
-				#send to nprocs-1
-		elif rank == nprocs - 1:
-			
+	plist = list(set(plist) - set(leaving_right) - set(leaving_left))	
 	
-		else:
+
+
+	if rank == 0:
+		for j in range(len(leaving_left)):
+			print(leaving_left[j].show_loc(),rank)
+			print("LALALAL")
+			leaving_left[j].move(float(P))
+			print(leaving_left[j].show_loc(),rank)
+		comm.send(leaving_right,dest = 1);
+		comm.send(leaving_left,dest = nprocs-1);
+		incoming_right = comm.recv(source = 1)
+		incoming_left = comm.recv(source = nprocs - 1);
+		plist = plist + incoming_right + incoming_left
+
+	elif rank == nprocs - 1 :
+		for j in range(len(leaving_right)):
+			print(leaving_right[j].show_loc(),rank)
+			print("LALALAL")
+			leaving_right[j].move(-float(P))
+			print(leaving_right[j].show_loc(),rank)	
+		comm.send(leaving_right,dest = 0);
+		comm.send(leaving_left,dest = nprocs-2);
+		incoming_right = comm.recv(source = 0)
+		incoming_left = comm.recv(source = nprocs - 2);
+		plist = plist + incoming_right + incoming_left
+	elif rank < (nprocs -1) and rank > 0 :
+		comm.send(leaving_right,dest = rank+1);
+		comm.send(leaving_left,dest = rank -1);
+		incoming_right = comm.recv(source = rank+1);
+		incoming_left = comm.recv(source = rank-1);
+		plist = plist + incoming_right + incoming_left
+	comm.barrier();
+	
+	
+
+	#tracking the position of given particle id
+	'''
+	for i in range(len(plist)):
+		if plist[i].show_pld() == target_particle:
+			print(str(plist[i].show_loc())+','+str(rank))
+	'''
+
+	
 	
