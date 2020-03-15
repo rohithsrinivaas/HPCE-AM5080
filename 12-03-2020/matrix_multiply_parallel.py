86a19277afe2c1ad4f048787733eff2bc90d6fc0
rohithsrinivaas <rohithsrinivaas@gmail.com>
2020-03-15 07:10:16 +0530

    Change in matrix multiply

--- 12-03-2020/matrix_multiply_parallel.py
+++ 12-03-2020/matrix_multiply_parallel.py
@@ -2,25 +2,19 @@ import math
 from mpi4py import MPI
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
-size = comm.Get_size()
+nProcs = comm.Get_size()
 import numpy as np
 import random
 
-np.set_printoptions(precision = 3)
-np.set_printoptions(suppress = True)
 
 if rank == 0 :
 	A = np.random.rand(12,4)
 	B = np.random.rand(4,6)
-	if(A.ndim != 2 or B.ndim != 2):
-		print("Error, more that 2 dimensions \n")
 	rows = np.shape(A)[0]
 	cols = np.shape(B)[1]
 	k = np.shape(A)[1]
-	print(rows)
-	print(cols)
-	print(k)
-	for i in range(size):
+	#Sending the shape of the array to different processors
+	for i in range(nProcs):
 		comm.send(rows, dest = i)
 		comm.send(cols, dest = i)
 
@@ -40,9 +34,9 @@ if rank == 0:
 			comm.send(c,dest = i*jprocs+j, tag = 1)
 r = comm.recv(source = 0, tag = 0)
 c = comm.recv(source = 0, tag = 1)
-prod = np.dot(r,c)
-print(rank, prod)
-comm.send(prod, dest = 0)
+comm.barrier()
+result = np.dot(r,c)
+comm.send(result, dest = 0)
 if rank == 0:
 	for i in range(iprocs):
 		for j in range(jprocs):
@@ -54,7 +48,9 @@ if rank == 0:
 		if i == 0:
 			C = row
 		else:
-			C = np.vstack((C,row))		
+			C = np.vstack((C,row))
+	print("matrix multiplication thru MPI")		
 	print(C)
 if rank == 0:
+	print("matrix multiplication thru numpy function")
 	print(np.dot(A,B))
